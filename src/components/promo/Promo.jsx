0a9e809a5ed56ca688832ff0163b2ac6177d77aa
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-10 16:21:59 +0300

    Added promo component

---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,16 @@
+import "./promo.css";
+
+const Promo = () => {
+  return (
+    <section className="promo">
+      <div className="container">
+        <div className="promo__content">
+          <div className="prom__text">TEXT</div>
+          <div className="promo__img">IMG</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Promo;
